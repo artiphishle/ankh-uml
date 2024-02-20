516de2df7e79de9a2a189bb19acc635d78b3bbbf
artiphishle <fabio.gartenmann@protonmail.com>
2024-02-20 08:30:05 +0100

    Fix NPX script

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+const path = require('path');
+const { spawn } = require('child_process');
+const scriptPath = path.join(__dirname, 'scripts', 'ankh-tools-uml.sh');
+const child = spawn(scriptPath, [], { stdio: 'inherit' });
+
+child.on('exit', (code, signal) => {
+  if (code !== 0) {
+    console.error(`Script execution failed with code ${code}`);
+  } else {
+    console.log('Script executed successfully');
+  }
+});
