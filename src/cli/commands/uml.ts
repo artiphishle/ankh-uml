e4641c4ac49da5b0818d80dabad7b35a12b771e4
artiphishle <fabio.gartenmann@protonmail.com>
2024-02-28 06:30:42 +0100

    Use gluegun CLI

---
+++ src/cli/commands/uml.ts
@@ -0,0 +1,19 @@
+import {GluegunCommand} from 'gluegun';
+import {AnkhUml} from 'src/tool/uml/Uml';
+import {ERenderer} from 'src/types/types';
+
+const command: GluegunCommand = {
+  name: 'uml',
+  run: async (toolbox) => {
+    const {parameters, print} = toolbox;
+
+    print.info('ankh-uml --rootFile={path/to/class.ts}');
+    print.info(parameters.options);
+
+    new AnkhUml()
+      .parse({rootFile: parameters.options.rootFile})
+      .render({renderer: ERenderer.PlantUml, outDir: './'});
+  },
+};
+
+module.exports = command;
